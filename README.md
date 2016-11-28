dd14474da4a233aa753bac417774c52e65336388
Marcel Link <marcel.link@t-online.de>
2016-11-28 15:07:21 +0100

    + application icon (made by Daria Cichon)

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-csr-generator
+[![npm version](https://badge.fury.io/js/csr-generator.svg)](https://badge.fury.io/js/csr-generator)
 
 https://cdn.rawgit.com/ml1nk/csr-generator/master/app/dist/index.html
---
+++ build/convert.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+rm icons/64x64.png
+rm icons/512x512.png
+rm icons/1024x1024.png
+rm icons/2048x2048.png
+
+convert background.png -resize 16x16 icons/16x16.png
+convert background.png -resize 32x32 icons/32x32.png
+convert background.png -resize 48x48 icons/48x48.png
+convert background.png -resize 128x128 icons/128x128.png
+convert background.png -resize 256x256 icons/256x256.png
+
+png2icns icon.icns icons/*.png
+
+convert background.png -resize 64x64 icons/64x64.png
+
+icotool -c icons/*.png -o icon.ico
+
+convert background.png -resize 512x512 icons/512x512.png
+convert background.png -resize 1024x1024 icons/1024x1024.png
+cp background.png icons/2048x2048.png
