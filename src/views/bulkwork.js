9379e3b482303eb7c6dca3938f8fd3a97d05a259
Marcel Link <marcel.link@t-online.de>
2016-12-15 21:12:13 +0100

    + bulk file processing

--- src/views/bulkwork.js
+++ src/views/bulkwork.js
@@ -1,8 +1,10 @@
 var $ = require("jquery");
 var filedata = require("./../lib/filedata.js");
 var formobject = require("./../lib/formobject.js");
-var windows1252 = require('windows-1252');
+var windows1252 = require("windows-1252");
+var jszip = require("jszip");
 var api = require("csr-helper");
+
 
 module.exports = function(views, main, data, overwriteBack) {
 
@@ -19,7 +21,7 @@ module.exports = function(views, main, data, overwriteBack) {
         } else {
           file.getData(function(success, data) {
               if (!success) {
-                  data.fileinput('clear');
+                  bulkfile.fileinput('clear');
                   $.toast({
                       heading: 'Error',
                       text: 'Es ist ein Fehler beim Auslesen der Bulk Datei aufgetreten.',
@@ -30,7 +32,34 @@ module.exports = function(views, main, data, overwriteBack) {
                   });
                   return;
               }
-              submit(windows1252.decode(data), formobject(form), views);
+              var form = formobject(form);
+              var result = api.import.bulk(windows1252.decode(data),form.OU1,form.OU2);
+              if(result===false) {
+                bulkfile.fileinput('clear');
+                $.toast({
+                    heading: 'Error',
+                    text: 'Die Bulk Datei enthält unvollständige Zeilen, bitte kontrollieren Sie die Anzahl der Spalten in jeder Zeile.',
+                    showHideTransition: 'fade',
+                    icon: 'error',
+                    position: 'top-right',
+                    hideAfter: 10000
+                });
+                return;
+              }
+              if(typeof result === "string") {
+                var error = result.split(":");
+                bulkfile.fileinput('clear');
+                $.toast({
+                    heading: 'Error',
+                    text: (error[0]==1 ? 'Die Bulk Datei enthält zwei identische ID\'s (erste Spalte): ' : 'Eines der Passwörter ist zu kurz (siebte Spalte): ') + error[1],
+                    showHideTransition: 'fade',
+                    icon: 'error',
+                    position: 'top-right',
+                    hideAfter: 10000
+                });
+                return;
+              }
+              submit(result, overwriteBack);
           });
           return false;
         }
@@ -39,8 +68,37 @@ module.exports = function(views, main, data, overwriteBack) {
     help();
 };
 
-function submit(data,form,views) {
-  console.log(api.import.bulk(data,form.OU1,form.OU2));
+function submit(result, overwriteBack) {
+  var wait = $("#zipWait");
+  $("#bulkworkForm").hide();
+  wait.show();
+
+  var zip = new jszip();
+  var promises = api.create.bulk(result);
+  var running = [];
+
+  for(var key in result) {
+      (function(folder, key){
+        folder.file("password.txt", result[key].password);
+        promises[key].then(function(data){
+          folder.file("request.csr",api.export.csr(data.csr));
+          folder.file("privateKey.pem",api.export.keypair.privateKey(data.keypair.privateKey,result[key].password));
+        });
+        running.push(promises[key]);
+      }(zip.folder(key),key));
+  }
+
+  Promise.all(running).then(function(){
+    zip.generateAsync({type:"blob"}).then(function(content) {
+      overwriteBack("Downloadbereich verlassen","Wurde das Ergebnis der Bulk Verarbeitung gesichert?","overview");
+      var time = Math.floor(new Date().getTime() / 1000);
+      $("#downloadZip").click(function() {
+          window.saveAs(content, "bulk_" + time + ".zip");
+      });
+      wait.hide();
+      $("#zipDownload").show();
+    });
+  });
 }
 
 
